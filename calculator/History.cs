6df632169eba4bbdfda1e58a058a715a52d3248f
srmagar2002 <167953839+srmagar2002@users.noreply.github.com>
2024-09-30 11:58:28 -0400

    history list

---
+++ calculator/History.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Operations
+    {
+        public float _fstoperand { get; set; }
+        public float _sndoperand { get; set; }
+
+        public char _char1 { get; set; }
+
+        public float _result { get; set; }
+
+    }
+    public class History
+    {
+        public List<History> history { get; set; }
+        public History()
+        {
+            history = new List<History>();
+        }
+    }
+}
